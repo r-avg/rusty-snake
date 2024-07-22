a16191e9acb4832b8c8a1371ed94210c323b41d4
Ramiro Angel Vega <02ravega@gmail.com>
2024-07-22 12:05:25 +0200

    window succesfully initialized

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .size(640, 480)
+        .title("gaem")
+        .build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::BLACK);
+    }
 }
